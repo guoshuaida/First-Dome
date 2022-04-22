e687d26844a170420af2857e062d83ab7464dadf
gsd <guoshuaida0329@163.com>
2022-04-22 21:24:20 +0800

    my frist commit

---
+++ hello.java
@@ -0,0 +1,11 @@
+hello java git!!!
+hello java git!!!
+hello java git!!!
+hello java git!!!
+hello java git!!!
+hello java git!!!
+hello java git!!!
+hello java git!!!
+public static void main(String args[]){
+System.out.println(123);
+}
